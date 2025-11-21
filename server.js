1db03970647facddb1f04a2e395ffb5ed8d0dfe5
dariabutkevich <152091207+dariabutkevich@users.noreply.github.com>
2025-11-21 23:59:41 +0300

    Serve index.html from root for full-stack

--- server.js
+++ server.js
@@ -73,7 +73,7 @@
 
 const express = require("express");
 const cors = require("cors");
-
+const path = require("path");
 const app = express();
 
 let corsOptions = {
@@ -85,7 +85,7 @@ let corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, '.')));
 const db = require("./app/models");
 const Role = db.role;
 
