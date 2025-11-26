02ac0c9eb51ff3bdfe7c7aeb1ea99900254c3d12
dariabutkevich <152091207+dariabutkevich@users.noreply.github.com>
2025-11-26 19:29:20 +0300

    Update index

--- tests/unit/auth.unit.test.js
+++ tests/unit/auth.unit.test.js
@@ -134,7 +134,7 @@ describe('Auth Controller Unit Tests', () => {
 
       return authController.refreshToken(mockReq, mockRes).then(() => {
         expect(mockRes.status).toHaveBeenCalledWith(403);
-        expect(mockRes.json).toHaveBeenCalledWith({ message: 'Refresh Token is required!' });
+        expect(mockRes.json).toHaveBeenCalledWith({ message: 'Refresh Token is!' });
       });
     }, 10000);
 
